348dc0e391f235373d675e11183686bd492e5e6a
Juanansc02 <juanan.2002.16@gmail.com>
2024-05-17 18:20:30 +0200

    Mass center substraction has been implemented. Now, both figures of the ToF are centered

--- Square_Environment.py
+++ Square_Environment.py
@@ -154,4 +154,60 @@ def represent_figure(ToF1, ToF2):
     plt.show()
     
 
-represent_figure(rounded_ToF1, rounded_ToF2_displaced)    
+#represent_figure(rounded_ToF1, rounded_ToF2_displaced)
+
+#Once the figure is represented, the MASS CENTER shall be calculated to obtain a reference
+def mass_center_substraction(ToF1, ToF2):
+    angles = [math.radians(i * added_angle) for i in range(len(ToF1))]
+    
+    #Initial square points:
+    initial_coordinates1 = [[ToF1[i] * math.sin(angles[i]), 
+                          ToF1[i] * math.cos(angles[i])] for i in range(0, len(ToF1))]
+    initial_coordinates2 = [[ToF2[j]*math.sin(angles[j]), 
+                          ToF2[j]*math.cos(angles[j])] for j in range(0, len(ToF2))]
+    
+    mass_center1 = [0, 0]
+    mass_center2 = [0, 0]
+    weights = [0, 0, 0, 0]
+
+    for i in range(0, len(ToF1)):
+        mass_center1[0] += initial_coordinates1[i][0] * (abs(initial_coordinates1[i][0] - initial_coordinates1[(i+1)%len(ToF1)][0]))
+        weights[0] += (abs(initial_coordinates1[i][0] - initial_coordinates1[(i+1)%len(ToF1)][0]))
+        mass_center1[1] += initial_coordinates1[i][1] * (abs(initial_coordinates1[i][1] - initial_coordinates1[(i+1)%len(ToF1)][1]))
+        weights[1] += (abs(initial_coordinates1[i][1] - initial_coordinates1[(i+1)%len(ToF1)][1]))
+        mass_center2[0] += initial_coordinates2[i][0] * (abs(initial_coordinates2[i][0] - initial_coordinates2[(i+1)%len(ToF2)][0]))
+        weights[2] += (abs(initial_coordinates2[i][0] - initial_coordinates2[(i+1)%len(ToF1)][0]))
+        mass_center2[1] += initial_coordinates2[i][1] * (abs(initial_coordinates2[i][1] - initial_coordinates2[(i+1)%len(ToF2)][1]))
+        weights[3] += (abs(initial_coordinates2[i][1] - initial_coordinates2[(i+1)%len(ToF1)][1]))
+
+    mass_center1[0] /= weights[0]
+    mass_center1[1] /= weights[1]
+    mass_center2[0] /= weights[2]
+    mass_center2[1] /= weights[3]
+
+    square_centered1 = []
+    square_centered2 = []
+
+    for j in range(0, len(ToF1)):
+        square_centered1.append([initial_coordinates1[j][0] - mass_center1[0], 
+                                initial_coordinates1[j][1] - mass_center1[1]])
+        square_centered2.append([initial_coordinates2[j][0] - mass_center2[0], 
+                                initial_coordinates2[j][1] - mass_center2[1]])
+
+    plt.plot(0, 0, 'bo')
+
+    plt.plot([square_centered1[i][0] for i in range(0, len(ToF1))], 
+             [square_centered1[i][1] for i in range(0, len(ToF1))], 'r-')
+    plt.plot([square_centered2[i][0] for i in range(0, len(ToF2))], 
+             [square_centered2[i][1] for i in range(0, len(ToF2))], 'g-')
+    #plt.plot((0, 0, 8, 8, 0), (0, 5, 5, 0, 0), 'b-')
+    print(initial_coordinates1)
+    plt.title('Detected environment')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.gca().set_aspect('equal')
+    plt.grid(True)
+    plt.show()
+    return square_centered1, square_centered2    
+
+sqr1, squr2 = mass_center_substraction(rounded_ToF1, rounded_ToF2_displaced)
