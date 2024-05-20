8bfbeb6d965de8e3e40ca2954e2222beca63713e
Juanansc02 <juanan.2002.16@gmail.com>
2024-05-20 19:33:43 +0200

    Open convex algorithm done

---
+++ Open Convex/open_convex.py
@@ -0,0 +1,300 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+num_samples = 200
+added_angle = 360 / num_samples
+sensor_distance_limit = 2
+
+robot_position1 = [0, 0]
+robot_position2 = [1, 4]
+
+initial_seed = [1.5, 3, -12]
+robot_position1[0] += initial_seed[0]
+robot_position1[1] += initial_seed[1]
+
+#Descent incrementals
+dx = 0.05
+dy= 0.05
+da = 0.2
+
+vertices = [(3, 0), (0, 3), (0, 6), (3, 9), (6, 9), (9, 6), (9, 3), (6, 0)]
+
+"""Functions to verify functionality of the algorithm"""
+
+def reduce_points(points, step=2): #   (number of points) / step = new number of points
+    reduced_points = points[::step]
+    # Ensure the figure is completed 
+    if reduced_points[-1] != points[-1]:
+        reduced_points.append(points[-1])
+    return reduced_points
+
+def rotate_matrix(matrix, theta):
+    theta = np.radians(theta)
+    cos_theta, sin_theta = np.cos(theta), np.sin(theta)
+    rotated_matrix = []
+    for (x, y) in matrix:
+        x_rot = x * cos_theta - y * sin_theta
+        y_rot = x * sin_theta + y * cos_theta
+        rotated_matrix.append((x_rot, y_rot))
+    return rotated_matrix
+
+def distance_btw_points(point1, point2):
+    return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)**0.5
+
+def reconstruct_figure_from_matrix(points1, points2):
+    points1.append(points1[0])
+    points2.append(points2[0])
+    points1 = np.array(points1)
+    points2 = np.array(points2)
+
+    plt.figure()
+    plt.plot(0, 0, 'bo', markersize = 1)
+    plt.plot(points1[:, 0], points1[:, 1], 'go', markersize = 1)  
+    plt.plot(points2[:, 0], points2[:, 1], 'bo', markersize = 1)  
+    plt.legend()
+    plt.axis('equal')
+    plt.show()
+
+def Tof2Points(ToF1, ToF2):
+    angles = np.linspace(0, 2 * np.pi, num_samples, endpoint=False)
+    
+    for i in range(0, len(ToF1)):
+        if ToF1[i] > sensor_distance_limit:
+            ToF1[i] = 0
+    for j in range(0, len(ToF2)):
+        if ToF2[j] > sensor_distance_limit:
+            ToF2[j] = 0  
+    
+    points_x1 = [distance * np.cos(angle) for distance, angle in zip(ToF1, angles)]
+    points_y1 = [distance * np.sin(angle) for distance, angle in zip(ToF1, angles)]
+    points_x2 = [distance * np.cos(angle) for distance, angle in zip(ToF2, angles)]
+    points_y2 = [distance * np.sin(angle) for distance, angle in zip(ToF2, angles)]
+
+    points1 = []
+    points2 = []
+
+    for x, y in zip(points_x1, points_y1):
+        points1.append((x, y))
+    for x, y in zip(points_x2, points_y2):
+        points2.append((x, y))
+    
+    points1 = [(x, y) for x, y in points1 if not (x == 0 and y == 0)]
+    points2 = [(x, y) for x, y in points2 if not (x == 0 and y == 0)]
+
+    return points1, points2
+
+def intersection_point_calc(robot_position, unitary_vector, vertex1, vertex2):
+    #Modify input data to numpy array for future calculations
+    robot_position = np.array(robot_position)
+    unitary_vector = np.array(unitary_vector)
+    vertex1 = np.array(vertex1)
+    vertex2 = np.array(vertex2)
+    
+    v1 = robot_position - vertex1
+    v2 = vertex2 - vertex1
+    v3 = np.array([-unitary_vector[1], unitary_vector[0]])
+    
+    t1 = np.cross(v2, v1) / np.dot(v2, v3) #Distance from point to segment. This value shall be finite and positive
+    t2 = np.dot(v1, v3) / np.dot(v2, v3) #Ensures the intersection ocurrs in the segment we are looking for
+    #print(np.dot(v1, v3))
+
+    if t1 >= 0 and 0 <= t2 <= 1:
+        return robot_position + t1 * unitary_vector
+    else:
+        return None
+
+def calculate_tof(robot_position, vertices):
+    distances = []
+    angles = np.linspace(0, 2 * np.pi, num_samples, endpoint=False) #list of angles (in radians) from 0 to 2*pi
+    
+    for angle in angles:
+        unitary_vector = np.array([np.cos(angle), np.sin(angle)]) #matrix with cosinus and sinus for each angle defined before
+        closest_distance = None
+        
+        for i in range(len(vertices)):
+            vertex1 = vertices[i]
+            vertex2 = vertices[(i + 1) % len(vertices)]
+            intersection_point = intersection_point_calc(robot_position, unitary_vector, vertex1, vertex2)
+            if intersection_point is not None:
+                distance = np.linalg.norm(intersection_point - np.array(robot_position))
+                #distance = np.linalg.norm(intersection_point - np.array(robot_position)) #substraction of final_position - initial_position to obtain the distance to the environment
+                if closest_distance is None or distance < closest_distance:
+                    closest_distance = distance
+        distances.append(closest_distance if closest_distance is not None else np.inf)
+    
+    return distances
+
+def ToF2Matrix(ToF):
+    angles = [math.radians(i * added_angle) for i in range(len(ToF))]
+    initial_coordinates = [[ToF[i] * math.cos(angles[i]), 
+                          ToF[i] * math.sin(angles[i])] for i in range(0, len(ToF))]
+    return initial_coordinates
+
+ToF1 = calculate_tof(robot_position1, vertices)
+ToF2 = calculate_tof(robot_position2, vertices)
+
+points1, points2 = Tof2Points(ToF1, ToF2)
+
+def interpolate_points(points):
+    # Determining minimum distance between points
+    distances = [np.linalg.norm(np.array(points[i]) - np.array(points[i+1])) for i in range(len(points)-1)]
+    minimum_distance = min(distances)
+    
+    interpolated_points = [points[0]]  
+    for i in range(len(points) - 1):
+        point1 = points[i]
+        point2 = points[i + 1]
+        # Distance between points
+        actual_distance = np.linalg.norm(np.array(point2) - np.array(point1))
+        
+        # Number of interpolations between points
+        num_interpolated_points = int(actual_distance / minimum_distance) - 1
+        
+        for j in range(1, num_interpolated_points + 1):
+            t = j / (num_interpolated_points + 1)
+            x_interpolated = point1[0] + t * (point2[0] - point1[0])
+            y_interpolated = point1[1] + t * (point2[1] - point1[1])
+            interpolated_points.append((x_interpolated, y_interpolated))
+        
+        # Add real points to maintain the data from the initial ToF vector
+        interpolated_points.append(point2)
+    
+    return interpolated_points
+
+figure1 = interpolate_points(points1)
+figure2 = rotate_matrix(interpolate_points(points2), initial_seed[2])
+
+#Do this to avoid problem with excess of points when interpolating 
+r1 = int(len(figure1) / len(figure2))
+r2 = int(len(figure2) / len(figure1))
+
+if (r1 > 1.5):
+    figure1 = reduce_points(figure1, r1)
+else:
+    figure2 = reduce_points(figure2, r2)
+
+"""
+Secondly, Housdorff distance has been defined to obtain the distance from ToF1 to ToF2
+"""
+def displaceX(points, dx):
+    new_points = []
+    for x, y in points:
+        new_points.append([x + dx, y]) 
+    return new_points
+
+def displaceY(points, dy):
+    new_points = []
+    for x, y in points:
+        new_points.append([x, y + dy])  
+    return new_points
+
+def hausdorff_distance(points1, points2): 
+    distance = 0
+    distance1 = 0
+    distance2 = 0
+    d1_min = float('inf')
+    d2_min = float('inf')
+    for i in points1:
+        d1_min = float('inf')
+        for j in points2:
+            distance = distance_btw_points(i, j)
+            if (d1_min > distance):
+                d1_min = distance
+        distance1 += d1_min
+    
+    for i in points2:
+        d2_min = float('inf')
+        for j in points1:
+            distance = distance_btw_points(i, j)
+            if (d2_min > distance):
+                d2_min = distance
+        distance2 += d2_min
+    
+    return ((distance1 + distance2) / (len(points1) + len(points2)))
+
+
+
+def displace_points(points, dx, dy):
+    return [(x + dx, y + dy) for x, y in points]
+
+def near_points(matrix1, matrix2, seed):
+    common_points1 = []
+    common_points2 = []
+
+    for point2 in matrix2:
+        if distance_btw_points([0, 0], point2) < ratio: common_points2.append(point2)
+    for point1 in matrix1:
+        if distance_btw_points(seed, point1) < ratio: common_points1.append(point1)
+
+    return common_points1, common_points2
+
+seed = [0, 0]  
+ratio = 2  
+
+figure1_p, figure2_p = near_points(figure1, figure2, seed) #Ultima modificacion
+
+
+""" Now, the main starts: """
+
+end = 0
+i = 0
+
+x_disp = 0
+y_disp = 0
+a_disp = 0
+
+while not end:
+    
+    d = hausdorff_distance(figure1_p, figure2_p)
+    d_plusX = hausdorff_distance(displaceX(figure1_p, dx), figure2_p)
+    d_minusX = hausdorff_distance(displaceX(figure1_p, -dx), figure2_p)
+    d_plusY = hausdorff_distance(displaceY(figure1_p, dy), figure2_p)
+    d_minusY = hausdorff_distance(displaceY(figure1_p, -dy), figure2_p)
+    d_plusA = hausdorff_distance(rotate_matrix(figure1_p, da), figure2_p)
+    d_minusA = hausdorff_distance(rotate_matrix(figure1_p, -da), figure2_p)
+    
+    if (d_minusX > d and d_plusX > d  and d_minusY > d and d_plusY > d and d_minusA > d and d_plusA > d or i > 150):
+        end = 1
+        break
+
+    if (d > d_plusX):
+        figure1_p = displaceX(figure1_p, dx)
+        figure1 = displaceX(figure1, dx)
+        seed[0] -= dx
+        x_disp -= dx
+    elif (d > d_minusX):
+        figure1_p = displaceX(figure1_p, -dx)
+        figure1 = displaceX(figure1, -dx)
+        seed[0] += dx
+        x_disp += dx
+    if (d > d_plusY):
+        figure1_p = displaceY(figure1_p, dy)
+        figure1 = displaceY(figure1, dy)
+        seed[1] -= dy
+        y_disp -= dy
+    elif (d > d_minusY):
+        figure1_p = displaceY(figure1_p, -dy)
+        figure1 = displaceY(figure1, -dy)
+        seed[1] += dy
+        y_disp += dy
+    if (d > d_plusA):
+        figure1_p = rotate_matrix(figure1_p, da)
+        figure1 = rotate_matrix(figure1, da)
+        a_disp -= da
+    elif (d > d_minusA):
+        figure1_p = rotate_matrix(figure1_p, -da)
+        figure1 = rotate_matrix(figure1, -da)
+        a_disp += da
+    
+    if i % 20 == 0:
+        print(len(figure1_p), len(figure2_p))
+        reconstruct_figure_from_matrix(figure1_p, figure2_p)
+    i += 1
+    print(f"Iteration {i}: [{x_disp}, {y_disp}, {a_disp}]")
+    seed[0] = -seed[0]
+    seed[1] = -seed[1]
+    figure1_p, figure2_p = near_points(figure1, figure2, seed)
+
+print("Gradient Descent finished")
+reconstruct_figure_from_matrix(figure1, figure2)
